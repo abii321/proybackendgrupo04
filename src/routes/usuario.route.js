5cdff1ed4e0a576dc3af82409921b3972174a981
Maximiliano Figueroa <marianof0101@gmail.com>
2026-06-29 22:30:26 -0300

    implementacion del endpoint de actualizacion de datos de usuario

--- src/routes/usuario.route.js
+++ src/routes/usuario.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const usuarioCtrl = require('../controllers/usuario.controller');
 
 router.get('/', usuarioCtrl.getUsuarios);
+router.put('/:id', usuarioCtrl.updateUsuario);
 
 module.exports = router;
