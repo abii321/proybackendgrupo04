5a7226bfb5d9f76dbece21bc93ea04cae32690e3
abii321 <abigail.teran321@gmail.com>
2026-07-07 17:32:07 -0300

    bloqueo de inicio de sesion si realiza mas de 5 intentos

--- src/routes/autenticacion.route.js
+++ src/routes/autenticacion.route.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const autenticacionCtrl = require('../controllers/autenticacion.controller');
+const loginLimiter = require('../middleware/loginIntentos');
 
 router.post('/signUp', autenticacionCtrl.signUpUsuario);
-router.post('/login', autenticacionCtrl.loginUsuario);
+router.post('/login', loginLimiter, autenticacionCtrl.loginUsuario);
 router.post('/signUpGoogle', autenticacionCtrl.signUpGoogle);
 router.post('/loginGoogle', autenticacionCtrl.loginGoogle);
 //router.post('/vincularGoogle', autenticacionCtrl.vincularGoogle);
