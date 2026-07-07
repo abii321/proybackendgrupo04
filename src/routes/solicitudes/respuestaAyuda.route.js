f350a4a923fb0c87ddbb2365f7c29f44679c2ef0
lujancansino <celestelujancansino@gmail.com>
2026-07-07 17:21:40 -0300

    mas seguridad jwt

--- src/routes/solicitudes/respuestaAyuda.route.js
+++ src/routes/solicitudes/respuestaAyuda.route.js
@@ -5,8 +5,6 @@ const authCtrl = require('./../../controllers/auth.controller');
 
 router.get('/solicitud/:solicitudId', authCtrl.verifyToken, respuestaCtrl.getRespuestasBySolicitud);
 
-// Proteger todas las rutas de respuestas (requiere sesión iniciada)
-router.use(authCtrl.verifyToken);
 
 router.post('/', authCtrl.verifyToken, respuestaCtrl.createRespuesta);
 router.put('/:id', authCtrl.verifyToken, respuestaCtrl.editRespuesta);
